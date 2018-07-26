3e334e88f6cd98df0ef4db643abaccc9876cbfc6
temitayo <tayo.adenmosun@gmail.com>
2018-07-26 14:18:04 +0100

    include contact page

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-
 ## Introduction
 * **rentals_template** home page of a film rental company.
 
 
 **Heroku page**
-`https://fierce-brook-17907.herokuapp.com/`
+`https://fierce-brook-17907.herokuapp.com/home.html`
---
+++ main.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+
+
+$GLOBALS['leapyears'] = 0;
+
+for ($year = 1980; $year <= 2018; $year++) {
+	if($year % 4 > 0 && $year % 400 > 0)
+  echo "$year <br>";
+	else
+     echo "$year Leap Year<br>";
+
+
+}
+
+for ($year = 1980; $year <= 2018; $year++) {
+	if($year % 4 == 0 && $year % 100 != 0 || $year % 400 == 0)
+		$leapyears++;
+}
+echo "$leapyears Leap Years";
+?>  
+
+</body>
+</html>
